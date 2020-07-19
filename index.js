16fd43f3bb247a2bb35b2d7365ed37d0ba1fb35d
Sondos Ahmed <sondosalshami@gmail.com>
2020-07-19 09:16:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your solution here!
-let append = ["Milo", "Otis", "Garfield"];
+const append = ["Milo", "Otis", "Garfield"];
       prepend = ["Milo", "Otis", "Garfield"];
       removeLast = ["Milo", "Otis", "Garfield"];
       removeFirst= ["Milo", "Otis", "Garfield"];
