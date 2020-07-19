ce94abe1bf57037d77a1cc4944c4d13a0753c9b2
Sondos Ahmed <sondosalshami@gmail.com>
2020-07-19 09:13:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ let append = ["Milo", "Otis", "Garfield"];
       removeLast = ["Milo", "Otis", "Garfield"];
       removeFirst= ["Milo", "Otis", "Garfield"];
 
-      append[append.length-1]= "Odie";
+      append[append.length]= "Odie";
